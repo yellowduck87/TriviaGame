a8f6667168ebdb5c70880a1665ea9484b8427046
Megan Evans <megan.evans.ducky@gmail.com>
2018-06-15 13:16:38 -0600

    started psudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+//create obects that contain:
+//identifier
+//--question string
+//--multipile choice string
+//-----a
+//-----b
+//-----c
+//-----d
+//--correct answer
+//--image src
+//--sound src
+
+//create a jQuery element that loads the question and options with a timer
+        //when timer is up, the correct answer is diplayed and the program moves to the next question
+        //when a selection is picked, the timer stops and goes toward answer screen--noting wether the choice was right or wrong.
+            //next question button
+        //if answerPicked is true----move to another jQuery element that compares the answer picked and the correct answer
