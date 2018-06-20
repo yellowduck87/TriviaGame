15a7d8a6d177a01aabbeca3461d1b7dc61d23388
Megan Evans <megan.evans.ducky@gmail.com>
2018-06-19 21:31:14 -0600

    added some code that displays one question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,15 +17,15 @@
 //if answerPicked is true----move to another jQuery element that compares the answer picked and the correct answer
 
 $(document).ready(function () {
-    
+
     var questions = {
         q1: {
-            qu: "question",
+            qu: "triva question",
             an: {
-                a: "one",
-                b: "two",
-                c: "three",
-                d: "four",
+                a: ": one",
+                b: ": two",
+                c: ": three",
+                d: ": four",
             },
             cor: "b",
             im: "#",
@@ -34,10 +34,10 @@ $(document).ready(function () {
         q2: {
             qu: "question",
             an: {
-                a: "one",
-                b: "two",
-                c: "three",
-                d: "four",
+                a: ": one",
+                b: ": two",
+                c: ": three",
+                d: ": four",
             },
             cor: "b",
             im: "#",
@@ -46,10 +46,10 @@ $(document).ready(function () {
         q3: {
             qu: "question",
             an: {
-                a: "one",
-                b: "two",
-                c: "three",
-                d: "four",
+                a: ": one",
+                b: ": two",
+                c: ": three",
+                d: ": four",
             },
             cor: "b",
             im: "#",
@@ -71,7 +71,7 @@ $(document).ready(function () {
 
     function start() {
         var startBtn = $("<button>");
-        startBtn.addClass("danger").attr("id", "start-button");
+        startBtn.addClass("btn btn-danger").attr("id", "start-button");
         startBtn.text("Start");
         $("#sub-head").append(startBtn);
     }
@@ -84,31 +84,37 @@ $(document).ready(function () {
         timer.attr("id", "begin");
     }
 
-
+    // console.log(questions[quest[counter]].an[0])
 
     function popQuiz() {
-        for (var i = 0; i < quest.length; i++) {
-            var options = Object.keys(questions[quest[i]].an)
-            var onDeck = questions[quest[i]];
-            var ask = questions[quest[i]].qu;
-            // var options = questions[quest[i]].an;
-            var correct = questions[quest[i]].cor;
-            var image = questions[quest[i]].im;
-            var sound = questions[quest[i]].sou;
 
-            if (answerChosen === false) {
-                for (var j = 0; j < options.length; j++) {
-                    var questList = $("<button>");
-                    questList.text(options[i]).addClass("what");
-                    $("#question-box").append(questList);
-                }
-            }
 
-            console.log(ask);
-            console.log(options);
-            // console.log("first option", options[0]);
-            // console.log(correct);
+        var questionDiv = $("<div>");
+        questionDiv.addClass("h1").attr("id", "ask-question");
+        questionDiv.append(questions[quest[counter]].qu);
+
+        var optionsDiv = $("<div>");
+        optionsDiv.attr("id", "options-list");
+
+    
+        $("#question-box").append(questionDiv).append(optionsDiv);
+
+        var options = Object.keys(questions[quest[counter]].an);
+        var values = Object.values(questions[quest[counter]].an);
+
+        for (var i = 0; i < 4; i++) {
+            var opt = $("<button>");
+            var optConcat = (options[i] + values[i]);
+            opt.addClass("btn btn-secondary").attr("id", [i]);
+            opt.append(optConcat);
+            $("#options-list").append(opt);
+            console.log(optConcat)
         }
+
+
+
+
+
     }
     popQuiz();
     // console.log(onDeck);
@@ -123,3 +129,26 @@ $(document).ready(function () {
         })
     }
 })
+
+// for (var i = 0; i < quest.length; i++) {
+//     var onDeck = questions[quest[i]];
+//     var ask = questions[quest[i]].qu;
+//     // var options = questions[quest[i]].an;
+//     var correct = questions[quest[i]].cor;
+//     var image = questions[quest[i]].im;
+//     var sound = questions[quest[i]].sou;
+// }
+//     if (answerChosen === false) {
+//         options.forEach( function (){
+//             var questList = $("<button>");
+//             questList.text(options[i]).addClass("what");
+//             $("#question-box").append(questList);
+//         })
+//         }
+
+
+
+// console.log(ask);
+// console.log(options);
+// console.log("first option", options[0]);
+// console.log(correct);
