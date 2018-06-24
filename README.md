a4a037d035f013563d5230508c7dfe03266756d0
Megan Evans <megan.evans.ducky@gmail.com>
2018-06-23 23:24:20 -0600

    all game code is working and most styling is in place

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TriviaGame
+
+This game was a labor of inches. Every time I thought I had something figured or in the bag, I was sorely disapointed. It is amazing how each little snippet of code interacts with seemingly endless other bits and bobs. A solid concept is key for anything to function at all, and one mustn't be afraid to recode or delete entire sections to get a program running. At least, that's what I feel I learned when doing this project. 
+
+The game should be fairly intuituive. It is a simple trivia quiz. The user has a set amount of time to answer a question, or the quiz moves on and the user gets the question wrong. 
+
+The game lets the user know if they got the question right or wrong immediately after selecting a button. It also tallies the total percentage correct and informs the user at the conclusion of the quiz. 
+
+Almost the entirtly of the HTML has been dynamically added to the DOM using jQuery. 
+
+This game was designed and coded by MEgan Evans
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,45 @@
 //when timer is up, the correct answer is diplayed and the program moves to the next question
 //when a selection is picked, the timer stops and goes toward answer screen--noting wether the choice was right or wrong.
-
-
 $(document).ready(function () {
 
     var questions = {
         q1: {
-            qu: "triva question 1",
+            qu: "How many different sounds does a chicken make to communicate?",
             an: {
-                a: ": one",
-                b: ": correct",
-                c: ": three",
-                d: ": four",
+                a: ": cluck, cluck",
+                b: ": correct More than 200 distinct cheeps and chirps.",
+                c: ": Almost 500 crazy chicken sounds!",
+                d: ": 27 sounds",
             },
             cor: 1,
-            im: "#",
+            corStr: "More than 200 distinct cheeps and chirps.",
+            im: "assets/images/chicken.jpg",
             sou: "#",
         },
         q2: {
-            qu: "trivia question 2",
+            qu: "Which bird is the smartest?",
             an: {
-                a: ": correct",
-                b: ": 2two",
-                c: ": 2three",
-                d: ": 2four",
+                a: ": correct a crow",
+                b: ": the dodo",
+                c: ": a mockingjay ",
+                d: ": the lyre bird",
             },
             cor: 0,
-            im: "#",
+            corStr: "A crow",
+            im: "assets/images/crow.jpg",
             sou: "#",
         },
         q3: {
-            qu: "trivia question 3",
+            qu: "How big is a hummingbird's egg?",
             an: {
-                a: ": 3one",
-                b: ": 3two",
-                c: ": correct",
-                d: ": 3four",
+                a: ": as big as a thumbnail",
+                b: ": the size of a grain of rice",
+                c: ": correct pea size",
+                d: ": 'this big'",
             },
             cor: 2,
-            im: "#",
+            corStr: "pea size",
+            im: "assets/images/hummingbird.png",
             sou: "#",
         }
     }
@@ -50,13 +51,14 @@ $(document).ready(function () {
 
     var intervalID;
     var currentTime;
+    var movingOn;
 
     var timer = {
-        timerCount: 2,
+        timerCount: 14,
         reset: function () {
             clearInterval(intervalID);
-            timer.timerCount = 2
-            $("#timer").text(this.timerCount + " seconds")
+            timer.timerCount = 14;
+            $("#timer").html("<h2>" + this.timerCount + " seconds<h2>")
         },
         start: function () {
             intervalID = setInterval(timer.count, 1000);
@@ -67,32 +69,35 @@ $(document).ready(function () {
         },
         count: function () {
             currentTime = timer.timerCount
-            if (currentTime == -1) {
+            if (counter < quest.length) {
                 if (currentTime == -1 && counter < quest.length) {
-                    timer.stop();
-                    $("#sub-head").empty();
-                    $("#question-box").html("game over")
+                    if (currentTime == -1 && counter == quest.length - 1) {
+                        wrong++;
+                        timer.stop();
+                        $("#sub-head").empty();
+                        $("#question-box").html("game over")
+                        incorrectSelectionGameOver();
+                        console.log("no selection game over")
 
-                } else {
-                    alert("times's up!");
-                    wrong++;
-                    answerChosen = true;
-                    timer.reset();
-                    nextQuestion();
+                    } else {
+                        wrong++;
+                        answerChosen = true;
+                        timer.reset();
+                        incorrectSelection();
+                        console.log("no-selection next question")
+                    }
+                }
+                if (currentTime != -1) {
+                    timer.timerCount--;
+                    $("#timer").html("<h2>" + currentTime + " seconds</h2>");
                 }
             }
-            if (currentTime != -1) {
-                timer.timerCount--;
-                $("#timer").text(currentTime + " seconds");
-                console.log(currentTime)
-            }
-
         },
     }
 
 
     function loadInstructions() {
-        $("#sub-head").html("'Choose the correct answer in the time allowed, or else...'<br>")
+        $("#sub-head").html("<h2>Choose the correct answer in the time allowed, or else...</h2>" + "<br><br>")
     }
     loadInstructions();
 
@@ -105,40 +110,32 @@ $(document).ready(function () {
 
     function makeTimer() {
         var timerDiv = $("<div>");
-        timerDiv.addClass("col-sm-12");
+        timerDiv.addClass("col-sm-12 h2");
         timerDiv.attr("id", "timer")
-        timerDiv.text(3 + " seconds");
+        timerDiv.text(15 + " seconds");
         timerDiv.append(timer);
         $("#sub-head").html(timerDiv);
-        // timer.reset();
-        // timer.start();
     }
 
     function clickStart() {
         $("#start-button").on("click", function () {
             popQuiz();
             makeTimer();
-            // timer.start();
-
-
         });
     }
-
-
 
     startButton();
     clickStart();
 
-
-
     function calculateScore() {
-        var totalScore = (right / (quest.length)) * 100
-        var disScore = ("You answered", totalScore + "%", "of the questions correctly")
+        var totalScore = Math.floor((right / (quest.length)) * 100);
+        var disScore = ("<br>" + "<h2>You answered " + totalScore + "%" + " of the questions correctly</h2>");
+        $("#question-box").append(disScore);
     }
 
     function displayQuestion() {
         timer.start();
-        var correct = questions[quest[counter]].cor;
+        var correct = parseInt(questions[quest[counter]].cor);
 
 
         var questionDiv = $("<div>");
@@ -147,9 +144,7 @@ $(document).ready(function () {
 
         var optionsDiv = $("<div>");
         optionsDiv.attr("id", "options-list");
-        var questAnswer = questionDiv.append(optionsDiv)
-        console.log(questions[quest[counter]].qu)
-        console.log(questionDiv)
+        var questAnswer = questionDiv.append(optionsDiv);
 
         $("#question-box").html(questAnswer);
 
@@ -163,77 +158,112 @@ $(document).ready(function () {
             opt.addClass("btn btn-secondary").attr("id", [i]);
             opt.append(optConcat);
             $("#options-list").append(opt).append("<br>");
-            console.log(optConcat)
-        }
-        console.log(currentTime)
-        if (parseInt(currentTime) == 0) {
-            alert("Too late");
-            wrong++;
-            answerChosen = true;
-            nextQuestion();
-        } else if (parseInt(currentTime) == 0 && counter < quest.length) {
-            alert("too late");
         }
 
         $(".btn-secondary").on("click", function (event) {
-            // console.log(answerChosen);
             var chosen = event.target.id;
-            console.log(chosen);
-            console.log(correct);
-            console.log(timer.timerCount)
+
 
             if (chosen != correct && counter < quest.length - 1) {
-                alert("boo, you chose incorrectly")
                 wrong++
                 answerChosen = true;
                 timer.reset();
-                nextQuestion();
+                incorrectSelection();
+
+                console.log("wrong answer next question")
 
             } else if (chosen == correct && counter < quest.length - 1) {
-                alert("yay, you chose correctly")
                 right++
                 answerChosen = true;
                 timer.reset();
-                nextQuestion();
+                correctSelection();
+
+                console.log("right answer next question")
 
             } else if (chosen == correct && counter < quest.length) {
-                alert("yay, you chose correctly");
                 right++;
                 timer.stop();
-                $("#sub-head").empty();
-                $("#question-box").html("game over")
+                correctSelectionGameOver();
+
+                console.log("right answer game over")
 
             } else if (chosen != correct && counter < quest.length) {
-                alert("boo, you chose incorrectly")
                 wrong++;
                 timer.stop();
-                $("#sub-head").empty();
-                $("#question-box").html("game over")
-
-
+                incorrectSelectionGameOver();
+                console.log("wrong answer game over")
             }
-
         });
-
     }
 
-
     function nextQuestion() {
+        clearInterval(nextQuestion);
         makeTimer();
         counter++;
         answerChosen = false;
-        console.log(counter)
         displayQuestion();
-    }
 
+    }
 
     function popQuiz() {
         $("#sub-head").html("<h1>Good Luck!</h1><br>");
         if (answerChosen === false) {
             displayQuestion();
-            console.log(answerChosen);
         }
 
     }
+
+    // var correctRes = "<h3>Yes! the correct answer was " + questions[quest[counter]].corStr + "</h3>";
+    // var incorrectRes = "<h3>Wrong, the correct answer was " + questions[quest[counter]].corStr + "<h3>";
+
+    function makeImg() {
+        var getImg = $("<img>");
+        getImg.attr("src", questions[quest[counter]].im).attr("id", "info-img");
+        $("#timer").append(getImg);
+    }
+
+
+    function correctSelection() {
+        setTimeout(nextQuestion, 3000);
+        $("#timer").empty();
+        $("#question-box").html("<h3>Yes! the correct answer was " + questions[quest[counter]].corStr + "</h3>");
+        makeImg();
+        console.log(questions[quest[counter]].im)
+    }
+
+    function correctSelectionGameOver() {
+        setTimeout(gameOver, 3000);
+        $("#timer").empty();
+        $("#question-box").html("<h3>Yes! the correct answer was " + questions[quest[counter]].corStr + "</h3>");
+        makeImg()
+        console.log(questions[quest[counter]].im)
+    }
+
+    function incorrectSelection() {
+        setTimeout(nextQuestion, 3000);
+        $("#timer").empty();
+        $("#question-box").html("<h3>Wrong, the correct answer was " + questions[quest[counter]].corStr + "<h3>");
+        makeImg();
+        console.log(questions[quest[counter]].im)
+    }
+
+    function incorrectSelectionGameOver() {
+        setTimeout(gameOver, 3000);
+        $("#timer").empty();
+        $("#question-box").html("<h3>Wrong, the correct answer was " + questions[quest[counter]].corStr + "<h3>");
+        makeImg();
+        console.log(questions[quest[counter]].im)
+    }
+
+
+
+    function gameOver() {
+        console.log("game over");
+        $("#sub-head").empty();
+        $("#question-box").html("<h1>Game Over</h1>");
+        calculateScore();
+        timer.stop();
+    }
+    
 
 });
